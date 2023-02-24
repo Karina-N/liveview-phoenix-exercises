7b02679f5182201b00513d10af4d1d22b04cec3f
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2023-02-24 17:00:36 +0100

    exercise -> assign attribute to function component

--- lib/live_view_studio_web/components/custom_components.ex
+++ lib/live_view_studio_web/components/custom_components.ex
@@ -2,18 +2,30 @@ defmodule LiveViewStudioWeb.CustomComponents do
   use Phoenix.Component
 
   # these declarations below apply only to the component that follow immediately
-  attr(:expiration, :integer, default: 24)
   slot(:legal)
   slot(:inner_block, required: true)
 
+  # here we display value that already is in the 'assigns' map (PART 1)
+  attr(:expiration, :integer, default: 24)
+
+  # here we display value that is NOT in the 'assigns' map (PART 1)
+  # attr(:minutes, :integer)
+
   def promo(assigns) do
+    # here we display value that already is in the 'assigns' map (PART 2)
+    assigns = assign(assigns, :minutes, assigns.expiration * 60)
+
+    # here we display value that is NOT in the 'assigns' map (PART 2)
+    # assigns = assign_new(assigns, :minutes, fn -> assigns.expiration * 60 end)
+
     ~H"""
     <div class="promo">
       <div class="deal">
         <%= render_slot(@inner_block) %>
       </div>
       <div class="expiration">
-        Deal expires in <%= @expiration %> hours
+        <%!-- Deal expires in <%= @expiration %> hours --%>
+        Deal expires in <%= @minutes %> minutes
       </div>
       <div class="legal">
         <%= render_slot(@legal) %>
